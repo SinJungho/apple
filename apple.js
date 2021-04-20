ea6ef4a021dfa479ae9a67814e7630fbcf825852
SinJungho <zmtls0516@naver.com>
2021-04-20 09:02:03 +0900

    first commit

---
+++ apple.js
@@ -0,0 +1,15 @@
+let b = alert("if like apple is ture or no");
+
+let a = prompt("Would you like apple?");
+
+// a.e
+
+for (let i = 0; i < 10; i++){
+    if(a == "yes"){
+        console.log("Wow we are same~");
+        i++;
+
+    } else if (a == "no"){
+        console.log("oh..we are not same");
+    }
+}
